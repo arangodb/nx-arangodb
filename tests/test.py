b4a50cc512fd584659ac2182dbc735a670423d93
Anthony Mahanna <anthony.mahanna@arangodb.com>
2024-04-26 20:28:44 -0400

    new: `test_bc`

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,17 @@
 import pytest
+import networkx as nx
+import nx_arangodb as nxadb
 
 from .conftest import db
 
+
 def test_db():
     assert db.version()
+
+
+def test_bc():
+    G_1 = nx.karate_club_graph()
+
+    G_2 = nxadb.Graph(G_1)
+
+    assert nx.betweenness_centrality(G_1) == nx.betweenness_centrality(G_2)

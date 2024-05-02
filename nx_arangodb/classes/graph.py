742633b7f91a36412aa3a021caadaab8ac91225b
Anthony Mahanna <anthony.mahanna@arangodb.com>
2024-05-01 22:06:35 -0400

    new: Graph `pull` method

--- nx_arangodb/classes/graph.py
+++ nx_arangodb/classes/graph.py
@@ -31,9 +31,12 @@ class Graph(nx.Graph):
         self.__graph_name = None
         self.__graph_exists = False
 
-        self.coo_use_cache = False
-        self.coo_load_parallelism = None
-        self.coo_load_batch_size = None
+        self.graph_loader_parallelism = None
+        self.graph_loader_batch_size = None
+
+        self.use_node_and_adj_dict_cache = False
+        self.use_coo_cache = False
+
         self.src_indices = None
         self.dst_indices = None
         self.vertex_ids_to_index = None
@@ -75,19 +78,19 @@ class Graph(nx.Graph):
             self.__db = db
             return
 
-        host = os.getenv("DATABASE_HOST")
-        username = os.getenv("DATABASE_USERNAME")
-        password = os.getenv("DATABASE_PASSWORD")
-        db_name = os.getenv("DATABASE_NAME")
+        self.__host = os.getenv("DATABASE_HOST")
+        self.__username = os.getenv("DATABASE_USERNAME")
+        self.__password = os.getenv("DATABASE_PASSWORD")
+        self.__db_name = os.getenv("DATABASE_NAME")
 
         # TODO: Raise a custom exception if any of the environment
         # variables are missing. For now, we'll just set db to None.
-        if not all([host, username, password, db_name]):
+        if not all([self.__host, self.__username, self.__password, self.__db_name]):
             self.__db = None
             return
 
-        self.__db = ArangoClient(hosts=host, request_timeout=None).db(
-            db_name, username, password, verify=True
+        self.__db = ArangoClient(hosts=self.__host, request_timeout=None).db(
+            self.__db_name, self.__username, self.__password, verify=True
         )
 
     def set_graph_name(self, graph_name: str | None = None):
@@ -112,3 +115,53 @@ class Graph(nx.Graph):
         else:
             self.__graph_exists = True
             print(f"Found graph '{self.__graph_name}' in the database")
+
+    def pull(self, load_node_and_adj_dict=True, load_coo=True):
+        if not self.graph_exists:
+            raise ValueError("Graph does not exist in the database")
+
+        adb_graph = self.db.graph(self.graph_name)
+
+        v_cols = adb_graph.vertex_collections()
+        edge_definitions = adb_graph.edge_definitions()
+        e_cols = {c["edge_collection"] for c in edge_definitions}
+
+        metagraph = {
+            "vertexCollections": {col: {} for col in v_cols},
+            "edgeCollections": {col: {} for col in e_cols},
+        }
+
+        from phenolrs.graph_loader import GraphLoader
+
+        kwargs = {}
+        if self.graph_loader_parallelism is not None:
+            kwargs["parallelism"] = self.graph_loader_parallelism
+        if self.graph_loader_batch_size is not None:
+            kwargs["batch_size"] = self.graph_loader_batch_size
+
+        result = GraphLoader.load(
+            self.db.name,
+            metagraph,
+            [os.environ["DATABASE_HOST"]],
+            username=os.environ["DATABASE_USERNAME"],
+            password=os.environ["DATABASE_PASSWORD"],
+            load_node_dict=load_node_and_adj_dict,
+            load_adj_dict=load_node_and_adj_dict,
+            load_coo=load_coo,
+            **kwargs,
+        )
+
+        if load_node_and_adj_dict:
+            # hacky, i don't like this
+            # need to revisit...
+            # consider using nx.convert.from_dict_of_dicts instead
+            self._node = result[0]
+            self._adj = result[1]
+
+        if load_coo:
+            self.src_indices = result[2]
+            self.dst_indices = result[3]
+            self.vertex_ids_to_index = result[4]
+
+    def push(self):
+        raise NotImplementedError("What would this look like?")

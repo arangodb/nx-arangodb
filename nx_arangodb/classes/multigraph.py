e7bb6d11c61ef0507a87b69b6fa6109d57902218
Anthony Mahanna <anthony.mahanna@arangodb.com>
2024-05-02 18:09:49 -0400

    temp: hide `MultiGraph` & `MultiDiGraph`

--- nx_arangodb/classes/multigraph.py
+++ nx_arangodb/classes/multigraph.py
@@ -1,32 +1,32 @@
-import networkx as nx
+# import networkx as nx
 
-import nx_arangodb as nxadb
-from nx_arangodb.classes.graph import Graph
+# import nx_arangodb as nxadb
+# from nx_arangodb.classes.graph import Graph
 
-networkx_api = nxadb.utils.decorators.networkx_class(nx.MultiGraph)
+# networkx_api = nxadb.utils.decorators.networkx_class(nx.MultiGraph)
 
-__all__ = ["MultiGraph"]
+# __all__ = ["MultiGraph"]
 
 
-class MultiGraph(nx.MultiGraph, Graph):
-    @classmethod
-    def to_networkx_class(cls) -> type[nx.MultiGraph]:
-        return nx.MultiGraph
+# class MultiGraph(nx.MultiGraph, Graph):
+#     @classmethod
+#     def to_networkx_class(cls) -> type[nx.MultiGraph]:
+#         return nx.MultiGraph
 
-    def __init__(
-        self,
-        *args,
-        **kwargs,
-    ):
-        super().__init__(*args, **kwargs)
+#     def __init__(
+#         self,
+#         *args,
+#         **kwargs,
+#     ):
+#         super().__init__(*args, **kwargs)
 
-        self.__db = None
-        self.__graph_name = None
-        self.__graph_exists = False
+#         self.__db = None
+#         self.__graph_name = None
+#         self.__graph_exists = False
 
-        self.coo_load_parallelism = None
-        self.coo_load_batch_size = None
+#         self.coo_load_parallelism = None
+#         self.coo_load_batch_size = None
 
-        self.set_db()
-        if self.__db is not None:
-            self.set_graph_name()
+#         self.set_db()
+#         if self.__db is not None:
+#             self.set_graph_name()

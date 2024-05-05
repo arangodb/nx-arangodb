2125a5710d2cf0d114796ce0cfdf71d4da600d35
Anthony Mahanna <anthony.mahanna@arangodb.com>
2024-05-05 02:31:20 -0400

    experimental: `CustomEdgeView`, `CustomEdgeDataView`

--- nx_arangodb/classes/reportviews.py
+++ nx_arangodb/classes/reportviews.py
@@ -1,4 +1,8 @@
+from __future__ import annotations
+
 import networkx as nx
+
+import nx_arangodb as nxadb
 
 
 class CustomNodeView(nx.classes.reportviews.NodeView):
@@ -38,3 +42,66 @@ class CustomNodeDataView(nx.classes.reportviews.NodeDataView):
         # want to return, instead of filtering it in Python
 
         ###########################
+
+
+class CustomEdgeDataView(nx.classes.reportviews.EdgeDataView):
+
+    ######################
+    # NOTE: Monkey Patch #
+    ######################
+
+    # Reason: We can utilize AQL to filter the data we
+    # want to return, instead of filtering it in Python
+    # This is hacky for now, but it's meant to show that
+    # the data can be filtered server-side.
+    # We solve this by relying on self._adjdict, which
+    # is the AdjListOuterDict object that has a custom
+    # items() method that can filter data with AQL.
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self._data and not isinstance(self._data, bool):
+            self._report = lambda n, nbr, dd: self._adjdict.items(
+                data=self._data, default=self._default
+            )
+
+    def __iter__(self):
+        if self._data and not isinstance(self._data, bool):
+            # don't need to filter data in Python
+            return self._report("", "", "")
+
+        return (
+            self._report(n, nbr, dd)
+            for n, nbrs in self._nodes_nbrs()
+            for nbr, dd in nbrs.items()
+        )
+
+
+class CustomEdgeView(nx.classes.reportviews.EdgeView):
+    dataview = CustomEdgeDataView
+
+    def __len__(self):
+
+        ######################
+        # NOTE: Monkey Patch #
+        ######################
+
+        # Old:
+        # num_nbrs = (len(nbrs) + (n in nbrs) for n, nbrs in self._nodes_nbrs())
+        # return sum(num_nbrs) // 2
+
+        # New:
+        G: nxadb.Graph = self._graph
+        return sum(
+            [
+                G.db.collection(ed["edge_collection"]).count()
+                for ed in G.adb_graph.edge_definitions()
+            ]
+        )
+
+        # Reason: We can utilize AQL to count the number of edges
+        # instead of making individual requests to the database
+        # i.e avoid having to do `n in nbrs` for each node
+
+        ######################

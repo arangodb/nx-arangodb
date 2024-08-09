d9861cc713b60d8675f5813ba8aec5a5e1cf2e16
Anthony Mahanna <anthony.mahanna@arangodb.com>
2024-08-08 21:50:10 -0400

    new: `test_multigraph_*_crud`
    minimal suite for now. need to revisit

--- tests/test.py
+++ tests/test.py
@@ -149,6 +149,7 @@ def test_shortest_path_remote_algorithm(load_graph: Any) -> None:
     [
         (nxadb.Graph),
         (nxadb.DiGraph),
+        (nxadb.MultiGraph),
     ],
 )
 def test_nodes_crud(load_graph: Any, graph_cls: type[nxadb.Graph]) -> None:
@@ -243,7 +244,7 @@ def test_nodes_crud(load_graph: Any, graph_cls: type[nxadb.Graph]) -> None:
 
     assert len(G_1.adj["person/1"]) > 0
     assert G_1.adj["person/1"]["person/2"]
-    edge_id = G_1.adj["person/1"]["person/2"]["_id"]
+    edge_id = "knows/1"
     G_1.remove_node("person/1")
     assert not db.has_document("person/1")
     assert not db.has_document(edge_id)
@@ -280,6 +281,7 @@ def test_graph_edges_crud(load_graph: Any) -> None:
 
     assert len(G_1.adj) == len(G_2.adj)
     assert len(G_1.edges) == len(G_2.edges)
+    assert G_1.number_of_edges() == G_2.number_of_edges()
 
     for src, dst, w in G_1.edges.data("weight"):
         assert G_1.adj[src][dst]["weight"] == w
@@ -426,6 +428,7 @@ def test_digraph_edges_crud(load_graph: Any) -> None:
 
     assert len(G_1.adj) == len(G_2.adj)
     assert len(G_1.edges) == len(G_2.edges)
+    assert G_1.number_of_edges() == G_2.number_of_edges()
 
     for src, dst, w in G_1.edges.data("weight"):
         assert G_1.adj[src][dst]["weight"] == w
@@ -565,6 +568,168 @@ def test_digraph_edges_crud(load_graph: Any) -> None:
     assert G_1["person/1"]["person/2"]["object"]["sub_object"]["foo"] == "bar"
     G_1["person/1"]["person/2"]["object"]["sub_object"]["foo"] = "baz"
     assert "_rev" not in G_1["person/1"]["person/2"]["object"]["sub_object"]
+    assert db.document(edge_id)["object"]["sub_object"]["foo"] == "baz"
+
+
+def test_multigraph_edges_crud(load_graph: Any) -> None:
+    G_1 = nxadb.MultiGraph(graph_name="KarateGraph")
+    G_2 = G_NX
+
+    assert len(G_1.adj) == len(G_2.adj)
+    assert len(G_1.edges) == len(G_2.edges)
+    assert G_1.number_of_edges() == G_2.number_of_edges()
+
+    for src, dst, w in G_1.edges.data("weight"):
+        assert G_1.adj[src][dst][0]["weight"] == w
+
+    for src, dst, w in G_1.edges.data("bad_key", default="boom!"):
+        assert "bad_key" not in G_1.adj[src][dst][0]
+        assert w == "boom!"
+
+    for k, edge_key_dict in G_1.adj["person/1"].items():
+        assert db.has_document(k)
+        assert db.has_document(edge_key_dict[0]["_id"])
+
+    G_1.add_edge("person/1", "person/1", foo="bar", _edge_type="knows")
+    edge_id = G_1.adj["person/1"]["person/1"][0]["_id"]
+    doc = db.document(edge_id)
+    assert doc["foo"] == "bar"
+    assert G_1.adj["person/1"]["person/1"][0]["foo"] == "bar"
+
+    del G_1.adj["person/1"]["person/1"][0]["foo"]
+    doc = db.document(edge_id)
+    assert "foo" not in doc
+
+    G_1.adj["person/1"]["person/1"][0].update({"bar": "foo"})
+    doc = db.document(edge_id)
+    assert doc["bar"] == "foo"
+
+    assert len(G_1.adj["person/1"]["person/1"][0]) == len(doc)
+    adj_count = len(G_1.adj["person/1"])
+    G_1.remove_edge("person/1", "person/1")
+    assert len(G_1.adj["person/1"]) == adj_count - 1
+    assert not db.has_document(edge_id)
+    assert "person/1" in G_1
+
+    assert not db.has_document(f"{G_1.default_node_type}/new_node_1")
+    col_count = db.collection(G_1.default_edge_type).count()
+
+    G_1.add_edge("new_node_1", "new_node_2", foo="bar")
+    assert db.document(G_1["new_node_1"]["new_node_2"][0]["_id"])["foo"] == "bar"
+    G_1.add_edge("new_node_1", "new_node_2", foo="bar", bar="foo")
+    doc = db.document(G_1["new_node_1"]["new_node_2"][1]["_id"])
+    assert doc["foo"] == "bar"
+    assert doc["bar"] == "foo"
+
+    bind_vars = {
+        "src": f"{G_1.default_node_type}/new_node_1",
+        "dst": f"{G_1.default_node_type}/new_node_2",
+    }
+
+    result = list(
+        db.aql.execute(
+            f"FOR e IN {G_1.default_edge_type} FILTER e._from == @src AND e._to == @dst RETURN e",  # noqa
+            bind_vars=bind_vars,
+        )
+    )
+
+    assert len(result) == 2
+
+    result = list(
+        db.aql.execute(
+            f"FOR e IN {G_1.default_edge_type} FILTER e._from == @dst AND e._to == @src RETURN e",  # noqa
+            bind_vars=bind_vars,
+        )
+    )
+
+    assert len(result) == 0
+
+    assert db.collection(G_1.default_edge_type).count() == col_count + 2
+    assert G_1.adj["new_node_1"]["new_node_2"][0]
+    assert G_1.adj["new_node_1"]["new_node_2"][0]["foo"] == "bar"
+    assert G_1.adj["new_node_2"]["new_node_1"][0]
+    assert (
+        G_1.adj["new_node_2"]["new_node_1"][0]["_id"]
+        == G_1.adj["new_node_1"]["new_node_2"][0]["_id"]
+    )
+    edge_id = G_1.adj["new_node_1"]["new_node_2"][0]["_id"]
+    doc = db.document(edge_id)
+    assert db.has_document(doc["_from"])
+    assert db.has_document(doc["_to"])
+    assert G_1.nodes["new_node_1"]
+    assert G_1.nodes["new_node_2"]
+
+    assert len(G_1.adj["new_node_1"]["new_node_2"]) == 2
+    G_1.remove_edge("new_node_1", "new_node_2")
+    G_1.clear()
+    assert "new_node_1" in G_1
+    assert "new_node_2" in G_1
+    assert "new_node_2" in G_1.adj["new_node_1"]
+    assert len(G_1.adj["new_node_1"]["new_node_2"]) == 1
+
+    G_1.add_edges_from(
+        [("new_node_1", "new_node_2"), ("new_node_1", "new_node_3")], foo="bar"
+    )
+    G_1.clear()
+    assert "new_node_1" in G_1
+    assert "new_node_2" in G_1
+    assert "new_node_3" in G_1
+
+    for k in G_1.adj["new_node_1"]["new_node_2"]:
+        assert G_1.adj["new_node_1"]["new_node_2"][k]["foo"] == "bar"
+
+    for k in G_1.adj["new_node_1"]["new_node_3"]:
+        assert G_1.adj["new_node_1"]["new_node_3"][k]["foo"] == "bar"
+
+    assert len(G_1.adj["new_node_1"]["new_node_2"]) == 2
+    assert len(G_1.adj["new_node_1"]["new_node_3"]) == 1
+    G_1.remove_edges_from([("new_node_1", "new_node_2"), ("new_node_1", "new_node_3")])
+    assert len(G_1.adj["new_node_1"]["new_node_2"]) == 1
+
+    assert "new_node_1" in G_1
+    assert "new_node_2" in G_1
+    assert "new_node_3" in G_1
+    assert "new_node_2" in G_1.adj["new_node_1"]
+    assert "new_node_3" not in G_1.adj["new_node_1"]
+
+    edge_id = "knows/1"
+    assert G_1["person/1"]["person/2"][edge_id] == G_1["person/2"]["person/1"][edge_id]
+    new_weight = 1000
+    G_1["person/1"]["person/2"][edge_id]["weight"] = new_weight
+    assert G_1["person/1"]["person/2"][edge_id]["weight"] == new_weight
+    assert G_1["person/2"]["person/1"][edge_id]["weight"] == new_weight
+    G_1.clear()
+    assert G_1["person/1"]["person/2"][edge_id]["weight"] == new_weight
+    assert G_1["person/2"]["person/1"][edge_id]["weight"] == new_weight
+
+    edge_id = G_1["person/1"]["person/2"][edge_id]["_id"]
+    G_1["person/1"]["person/2"][edge_id]["object"] = {"foo": "bar", "bar": "foo"}
+    assert "_rev" not in G_1["person/1"]["person/2"][edge_id]["object"]
+    assert isinstance(G_1["person/1"]["person/2"][edge_id]["object"], EdgeAttrDict)
+    assert db.document(edge_id)["object"] == {"foo": "bar", "bar": "foo"}
+
+    G_1["person/1"]["person/2"][edge_id]["object"]["foo"] = "baz"
+    assert db.document(edge_id)["object"]["foo"] == "baz"
+
+    del G_1["person/1"]["person/2"][edge_id]["object"]["foo"]
+    assert "_rev" not in G_1["person/1"]["person/2"][edge_id]["object"]
+    assert isinstance(G_1["person/1"]["person/2"][edge_id]["object"], EdgeAttrDict)
+    assert "foo" not in db.document(edge_id)["object"]
+
+    G_1["person/1"]["person/2"][edge_id]["object"].update(
+        {"sub_object": {"foo": "bar"}}
+    )
+    assert "_rev" not in G_1["person/1"]["person/2"][edge_id]["object"]["sub_object"]
+    assert isinstance(
+        G_1["person/1"]["person/2"][edge_id]["object"]["sub_object"], EdgeAttrDict
+    )
+    assert db.document(edge_id)["object"]["sub_object"]["foo"] == "bar"
+
+    G_1.clear()
+
+    assert G_1["person/1"]["person/2"][edge_id]["object"]["sub_object"]["foo"] == "bar"
+    G_1["person/1"]["person/2"][edge_id]["object"]["sub_object"]["foo"] = "baz"
+    assert "_rev" not in G_1["person/1"]["person/2"][edge_id]["object"]["sub_object"]
     assert db.document(edge_id)["object"]["sub_object"]["foo"] == "baz"
 
 

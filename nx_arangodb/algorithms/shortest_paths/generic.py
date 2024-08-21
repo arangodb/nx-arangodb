91ceaf5922945d9f9c236e42d7b8627decf808c1
Anthony Mahanna <anthony.mahanna@arangodb.com>
2024-08-21 12:27:58 -0400

    fix: shortest path `source` & `target`

--- nx_arangodb/algorithms/shortest_paths/generic.py
+++ nx_arangodb/algorithms/shortest_paths/generic.py
@@ -35,6 +35,12 @@ def shortest_path(
     if method != "dijkstra":
         raise NotImplementedError("Only dijkstra method is supported")
 
+    if isinstance(source, int):
+        source = G.nodes[source]["_id"]
+
+    if isinstance(target, int):
+        target = G.nodes[target]["_id"]
+
     query = """
         FOR vertex IN ANY SHORTEST_PATH @source TO @target GRAPH @graph
         OPTIONS {'weightAttribute': @weight}

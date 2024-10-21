1e7df83cb1a50001aa86abea6e520a50251999c3
Anthony Mahanna <43019056+aMahanna@users.noreply.github.com>
2024-10-21 11:29:51 -0400

    bump: networkx dep (#64)
    * bump: networkx dep
    
    * fix: `mypy`
    
    * new: `_should_backend_run`
    
    overriding https://github.com/networkx/networkx/blob/networkx-3.4.1/networkx/utils/backends.py#L1514-L1535 to support backwards compatibility
    
    * Update VERSION
    
    * fix: use `from_networkx` if graph does not exist in db
    
    * remove demo.py
    
    * new: `_should_backend_run`, `_can_backend_run`
    
    * fix: lint

--- nx_arangodb/convert.py
+++ nx_arangodb/convert.py
@@ -138,11 +138,11 @@ if GPU_AVAILABLE:
             return G
 
         if isinstance(G, nxadb.Graph):
-            if not G.graph_exists_in_db:
-                m = "nx_arangodb.Graph does not exist in ArangoDB. Cannot pull graph."
-                raise ValueError(m)
-
             logger.debug("converting nx_arangodb graph to nx_cugraph graph")
+
+            if not G.graph_exists_in_db:
+                return nxcg.convert.from_networkx(G)
+
             return nxadb_to_nxcg(G, as_directed=as_directed)
 
         raise TypeError(f"Expected nx_arangodb.Graph or nxcg.Graph; got {type(G)}")

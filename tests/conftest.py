a60cd852f07df865931789fab07317234e06b350
Anthony Mahanna <anthony.mahanna@arangodb.com>
2024-11-24 21:54:20 -0500

    temp: lock adb
    need to investigate issues with 3.12.3

--- tests/conftest.py
+++ tests/conftest.py
@@ -42,12 +42,14 @@ def pytest_configure(config: Any) -> None:
     print("Username: " + con["username"])
     print("Password: " + con["password"])
     print("Database: " + con["dbName"])
-    print("----------------------------------------")
 
     global db
     db = ArangoClient(hosts=con["url"]).db(
         con["dbName"], con["username"], con["password"], verify=True
     )
+
+    print("Version: " + db.version())
+    print("----------------------------------------")
 
     os.environ["DATABASE_HOST"] = con["url"]
     os.environ["DATABASE_USERNAME"] = con["username"]
